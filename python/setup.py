038a7ef0a1128fb4f340d9c871a2d8bf6dc3b0d3
AeZG <gregorydyuldin@gmail.com>
2025-05-27 15:47:29 +0300

    Edit version

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chaika-proto",
-    version="0.3.0",
+    version="0.5.0",
     packages=find_packages(where="python"),
     package_dir={"": "python"},
     install_requires=[
